55cf51fc133f788f5df52862eaea466a9abf2d08
Nakagawa.T <93106062+CakeonRice@users.noreply.github.com>
2022-05-11 10:30:12 +0900

    beta

---
+++ prototype - 射撃判定あり (3)/prototype - 射撃判定あり/prototype/Assets/SceneTransitions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Button_B"))
+        {
+            SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+        }
+        if(Input.GetKey(KeyCode.B))
+        {
+            SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ prototype - 射撃判定あり (3)/prototype - 射撃判定あり/prototype/Assets/Scenes/Script/OnGameClear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnGameClear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetButtonDown("Button_X"))
+        {
+            SceneManager.LoadScene("StageSelectScene", LoadSceneMode.Single);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            SceneManager.LoadScene("StageSelectScene", LoadSceneMode.Single);
+        }
+    }
+}
